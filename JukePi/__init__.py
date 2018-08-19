196a5be7cd641327eb2d14f9944dd61dcf90fecf
jaivalis <johnaivalis@gmail.com>
2018-08-19 15:09:18 +0200

    Added logging

---
+++ JukePi/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import logging
+import sys
+
+log_format = '%(asctime)s %(levelname)s %(name)s | %(message)s'
+logging.basicConfig(stream=sys.stdout, level=logging.INFO, format=log_format)
+
+app = Flask(__name__)
+
+from JukePi.dao import DB
+
+database = DB()
+database.create_all()
