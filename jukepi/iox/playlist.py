3180160afbf38011b028fba21fa7a55ea0d4e226
jaivalis <john@axual.io>
2019-04-16 19:48:47 +0200

    Added return types

--- jukepi/iox/playlist.py
+++ jukepi/iox/playlist.py
@@ -1,25 +1,28 @@
+from jukepi.db.models import Track
+
+
 class Playlist(object):
     
     def __init__(self, tracks):
         self.tracks = tracks
         self.index = 0
     
-    def enqueue(self, track):
+    def enqueue(self, track) -> None:
         self.tracks.append(track)
     
-    def remove(self, track):
+    def remove(self, track) -> None:
         # TODO remove removes the first matching value, not a specific index
         # del removes the item at a specific index:
         self.tracks.remove(track)
     
-    def move_up(self, track):
+    def move_up(self, track) -> None:
         pass
     
-    def move_down(self, track):
+    def move_down(self, track) -> None:
         pass
 
-    def get_song_uris(self):
+    def get_song_uris(self) -> list:
         return [track.uri for track in self.tracks]
     
-    def get_song(self, index):
+    def get_song(self, index) -> Track:
         return self.tracks[index]
